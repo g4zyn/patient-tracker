5bbd3c9615b3b14e1052f26b0e532d4659b44b02
Marko <markogajiin@gmail.com>
2020-04-17 21:54:06 +0200

    login form created

--- app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/activity/LoginActivity.kt
+++ app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/activity/LoginActivity.kt
@@ -1,17 +1,13 @@
 package rs.raf.projekat1.marko_gajin_RM8517.view.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import rs.raf.projekat1.marko_gajin_RM8517.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
