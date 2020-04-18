24a2d11b4e1181741316a55b41c7730870063648
Marko <markogajiin@gmail.com>
2020-04-18 19:51:47 +0200

    fix

--- app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/models/User.kt
+++ app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/models/User.kt
@@ -1,7 +1,3 @@
 package rs.raf.projekat1.marko_gajin_RM8517.models
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class User (val firstName: String, val lastName: String, val hospital: String) : Parcelable {}
+class User (val firstName: String, val lastName: String, val hospital: String) {}
