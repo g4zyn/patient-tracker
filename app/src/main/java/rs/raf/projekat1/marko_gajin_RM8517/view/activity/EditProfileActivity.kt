217d551abfa5a9b554cb341f8303fcc550ac1997
Marko <markogajiin@gmail.com>
2020-05-07 20:26:38 +0200

    edit user's profile layout

---
+++ app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/activity/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package rs.raf.projekat1.marko_gajin_RM8517.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import rs.raf.projekat1.marko_gajin_RM8517.R
+
+class EditProfileActivity : AppCompatActivity(R.layout.activity_edit_profile) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
