072f272a1e4ebfe38c40bd594c9c1e0ede2a161b
Marko <markogajiin@gmail.com>
2020-05-09 23:07:34 +0200

    Get number of patients based on their status

--- app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/fragments/StatusFragment.kt
+++ app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/fragments/StatusFragment.kt
@@ -1,10 +1,27 @@
 package rs.raf.projekat1.marko_gajin_RM8517.view.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import kotlinx.android.synthetic.main.fragment_status.*
 import rs.raf.projekat1.marko_gajin_RM8517.R
+import rs.raf.projekat1.marko_gajin_RM8517.viewmodels.PatientViewModel
 
 class StatusFragment : Fragment(R.layout.fragment_status) {
 
+    private val patientViewModel: PatientViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+//    TODO get filtered list of patients based on their status
+    private fun init() {
+        waitingRoom.text = patientViewModel.getPatients().value?.size.toString()
+        hospitalized.text = patientViewModel.getPatients().value?.size.toString()
+        released.text = patientViewModel.getPatients().value?.size.toString()
+    }
 
 }
