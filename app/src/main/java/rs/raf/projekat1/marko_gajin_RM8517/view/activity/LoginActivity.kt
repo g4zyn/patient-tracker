8d92fbbec160a27c186936a670910f8c961dfea8
Marko <markogajiin@gmail.com>
2020-05-10 23:13:01 +0200

    Validate input before log in

--- app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/activity/LoginActivity.kt
+++ app/src/main/java/rs/raf/projekat1/marko_gajin_RM8517/view/activity/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import rs.raf.projekat1.marko_gajin_RM8517.R
 import rs.raf.projekat1.marko_gajin_RM8517.models.User
@@ -28,10 +29,48 @@ class LoginActivity : AppCompatActivity(R.layout.activity_login) {
             val hospital = hospitalEt.text.toString()
             val pinCode = passwordEt.text.toString()
 
-            if (pinCode == PIN_CODE) {
+            if (isInputValid(firstName, lastName, hospital) && isValidPin(pinCode)) {
                 login(User(firstName, lastName, hospital))
             }
         }
+    }
+
+    private fun isInputValid(firstName: String, lastName: String, hospital: String): Boolean {
+        when {
+            firstName == "" -> {
+                Toast.makeText(applicationContext, "Name field can't be empty!", Toast.LENGTH_SHORT).show()
+            }
+            lastName == "" -> {
+                Toast.makeText(applicationContext, "Surname field can't be empty!", Toast.LENGTH_SHORT).show()
+            }
+            hospital == "" -> {
+                Toast.makeText(applicationContext, "Hospital field can't be empty!", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private fun isValidPin(pinCode: String): Boolean {
+        when {
+            pinCode == "" -> {
+                Toast.makeText(applicationContext, "Enter pin code!", Toast.LENGTH_SHORT).show()
+            }
+            pinCode.length != 4 -> {
+                Toast.makeText(applicationContext, "Invalid pin code length!", Toast.LENGTH_SHORT).show()
+            }
+            pinCode != PIN_CODE -> {
+                Toast.makeText(applicationContext, "Invalid pin!", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                return true
+            }
+        }
+
+        return false
     }
 
     private fun login(user:User) {
